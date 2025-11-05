1f03ebc4ea112b31629c6207a48b18c7cc29ed5c
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-11-05 16:18:26 +0300

    Implementing smooth scrolling

--- script.js
+++ script.js
@@ -30,6 +30,23 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+
+  // scrolling - old way
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // new way
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 ///////////////////////////////////////
 
 // SELECTING, CREATING, AND DELETING ELEMENTS
