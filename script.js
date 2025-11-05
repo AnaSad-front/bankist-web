bf30c1f200bacbb4a477337203715f78d65388b3
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-11-05 16:52:04 +0300

    types of events and event handlers

--- script.js
+++ script.js
@@ -139,3 +139,21 @@ console.log(logo.classList.contains('c')); // not includes()
 // Don't use
 // logo.className = 'jonas';
 */
+
+// TYPES OF EVENTS AND EVENT HANDLERS
+/*
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 10000);
+
+// Old way
+// h1.onmouseenter = function (e) {
+//   alert('You are reading the heading :D');
+// };
+*/
