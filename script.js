f43d3787ae52c95af43848085a7a09ce817674ec
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-11-06 14:11:38 +0300

    event delegation: implementing page navigation

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -30,9 +32,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
+///////////////////////////////////////
+// SMOOTH SCROLLING
 btnScrollTo.addEventListener('click', function (e) {
   // const s1coords = section1.getBoundingClientRect();
 
@@ -45,6 +46,22 @@ btnScrollTo.addEventListener('click', function (e) {
 
   // new way
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//////////////////////////////////////
+// PAGE NAVIGATION
+
+// event delegation
+// 1. add event listener to common parent element
+// 2. determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
 
 ///////////////////////////////////////
